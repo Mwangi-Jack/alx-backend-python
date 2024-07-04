196058e782925e602d6b0da382cadf24137e359b
Mwangi-Jack <jasonmuturi2000@gmail.com>
2024-07-04 15:54:11 +0300

    type annotation for an iterable

---
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+"""Duck typing an iterable object"""
+
+from typing import Iterable, Sequence, List, Tuple
+
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
+    """This function takes in a list"""
+    return [(i, len(i)) for i in lst]
