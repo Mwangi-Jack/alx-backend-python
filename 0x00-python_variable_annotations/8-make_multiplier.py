78f4a91957ee56f05fdc51987dc83a53821e9cca
Mwangi-Jack <jasonmuturi2000@gmail.com>
2024-07-04 15:47:02 +0300

    type annotation for a function returning a function

---
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+"""Make multiplier"""
+
+from typing import Callable
+
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    """
+    This function takes a float 'multiplier' as argument
+    and returns a function that multiplies a float by 'multiplier'
+    """
+    def multiply(x: float) -> float:
+        return x * multiplier
+
+    return multiply
