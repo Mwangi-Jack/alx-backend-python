c5d6d71c008880e86f6d47129471696e769cecca
Mwangi-Jack <jasonmuturi2000@gmail.com>
2024-07-04 19:43:39 +0300

    duck typing

--- 0x00-python_variable_annotations/100-safe_first_element.py
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -1,11 +1,10 @@
 #!/usr/bin/env python3
 """Duck typing"""
 
-from types import NoneType
 from typing import Sequence, Any, Union
 
 
-def safe_first_element(lst: Sequence[Any]) -> Union[Any, NoneType]:
+def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
     """Duck typing"""
     if lst:
         return lst[0]
