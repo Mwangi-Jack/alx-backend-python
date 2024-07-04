2663d9af68a2683e0da1144eb670dc284fb6f72b
Mwangi-Jack <jasonmuturi2000@gmail.com>
2024-07-04 17:49:07 +0300

    duck typing using typeVar

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -6,8 +6,9 @@ from typing import Mapping, Any, Union, TypeVar
 
 T = TypeVar('T')
 
+
 def safely_get_value(dct: Mapping, key: Any,
-                     default: Union[T, NoneType] = None) -> Union[Any, T] :
+                     default: Union[T, NoneType] = None) -> Union[Any, T]:
     """safely get a value"""
 
     if key in dct:
