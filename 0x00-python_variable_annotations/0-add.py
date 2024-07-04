4529d7271d4069976058be5b68b00add644aece8
Mwangi-Jack <jasonmuturi2000@gmail.com>
2024-07-04 14:01:38 +0300

    type annotated function

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""type annotatated function"""
+
+
+def add(a: int, b: int) -> float:
+    """
+    This function takes in two variables of type int
+    and returns a sum of type float
+    """
+    return a + b
