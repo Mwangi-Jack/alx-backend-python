cf9c96644ddf08e9097e12bd7971d9c7c64fb835
Mwangi-Jack <jasonmuturi2000@gmail.com>
2024-07-04 14:33:39 +0300

    type annotated function returning int

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""floor function annotation"""
+
+import math
+
+
+def floor(n: float) -> int:
+    """
+    This function takes in a float variable
+    and returns a floor of the variable of type int
+    """
+    return math.floor(n)
