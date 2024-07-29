447baa33a327fa050704405b98e4ac4558d8035e
Mwangi-Jack <jasonmuturi2000@gmail.com>
2024-07-29 12:31:02 +0300

    mocking HTTP calls

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -2,9 +2,10 @@
 """Unittests and Intergration Tests"""
 
 import unittest
-from unittest.mock import patch, Mock
+from unittest.mock import patch, MagicMock
+
 from parameterized import parameterized
-from utils import access_nested_map, get_json
+from utils import access_nested_map, get_json, memoize
 
 
 class TestAccessNestedMap(unittest.TestCase):
@@ -34,15 +35,34 @@ class TestGetJson(unittest.TestCase):
         ('https://example.com', {'payload': True}),
         ('https://holberton.io', {'payload': False})
     ])
-    @patch('requests.get')
-    def test_get_json(self, url, payload, mock_get):
+    @patch('utils.requests')
+    def test_get_json(self, set_url, expected_payload, mock_get):
         """Test for getting JSON"""
 
-        mock_response = Mock()
-        mock_response.json.return_value = payload
-        mock_get.return_value = mock_response
-        result = get_json(url)
+        mock_response = MagicMock(return_value=200)
+        mock_response.json.return_value = expected_payload
+        mock_get.get.return_value = mock_response
+        result = get_json(set_url)
 
         self.assertIsNotNone(result)
-        self.assertEqual(result, payload)
-        mock_get.assert_called_once_with(url)
+        self.assertEqual(result, expected_payload)
+        mock_get.get.assert_called_once_with(set_url)
+
+# class TestMemoize(unittest.TestCase):
+#     """This class defines methods to test the memoize decorator"""
+
+#     def test_memoize(self):
+#         """This method tests the memoize decorator"""
+#         class TestClass:
+#             def a_method(self):
+#                 return 42
+
+#             @memoize
+#             def a_property(self):
+#                 return self.a_method
+
+
+
+
+if __name__ == '__main__':
+    unittest.main()
