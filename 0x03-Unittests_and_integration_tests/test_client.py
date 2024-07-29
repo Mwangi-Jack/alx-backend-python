dd8d281ce4304807f83baa8762e5d5765ee11d99
Mwangi-Jack <jasonmuturi2000@gmail.com>
2024-07-29 16:42:31 +0300

    mocking a property

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -31,14 +31,18 @@ class TestGithubOrgClient(unittest.TestCase):
         """
         This method tests the _public_repos_url method of the client
         """
-        expected_org = {'repos_url': 'https://api.github.com/orgs/google/repos'}
+        expected_org = {
+            'repos_url': 'https://api.github.com/orgs/google/repos'
+            }
 
-        with patch.object(GithubOrgClient, 'org', new_callable=PropertyMock) as mock_org:
+        with patch.object(GithubOrgClient, 'org',
+                          new_callable=PropertyMock) as mock_org:
             mock_org.return_value = expected_org
             client = GithubOrgClient('google')
             result = client._public_repos_url
 
             self.assertEqual(result, expected_org['repos_url'])
+
 
 if __name__ == '__main__':
     unittest.main()
